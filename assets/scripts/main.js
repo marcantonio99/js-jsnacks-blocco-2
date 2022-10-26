6e93187d36b8ee25ffdd0864a41439c3e8789e0d
marcantonio99 <marcantonio8799@gamil.com>
2022-10-26 10:14:57 +0200

    JSnack 2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+
 //Chiedi all'utente di inserire un numero, se pari stampa, se dispari stampa il numero successivo:
 
 const number = parseInt(prompt('inserisci un numero'));
@@ -5,4 +6,22 @@ if((number % 2) != 1){
     console.log('il numero stampato è: '+ number );
 }else((number % 2) != 0);{
     console.log('il numero stampato è: '+ (number + 1) );
+}
+
+
+//Generatore di nomi e cognomi casuale. Crea falsa lista di invitati:
+
+const names = ['Mario', 'Emanuela', 'Fabio'];
+const surnames = ['Bros', 'Marangia', 'Esposito'];
+console.log(names);
+
+const fullNames = [];
+
+let nameRandom = Math.floor(Math.random() * names.leght);
+let surnameRandom = Math.floor(Math.random() * surnames.leght);
+
+console.log(`il tuo random: ${names[nameRandom]} ${surnames[surnameRandom]}`);
+
+for ( let i = 0; i < surnames.length; i++ ){
+
 }
